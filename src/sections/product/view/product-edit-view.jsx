dd117a4baea989a429f733b75d858cefbb542cf3
Mrafisyahbani77 <mrafi.syahbani@gmail.com>
2025-04-19 20:07:50 +0700

    feat: fix bug deploy vercel

--- src/sections/product/view/product-edit-view.jsx
+++ src/sections/product/view/product-edit-view.jsx
@@ -3,8 +3,6 @@ import PropTypes from 'prop-types';
 import Container from '@mui/material/Container';
 // routes
 import { paths } from 'src/routes/paths';
-// api
-import { useGetProduct } from 'src/api/product';
 // components
 import { useSettingsContext } from 'src/components/settings';
 import CustomBreadcrumbs from 'src/components/custom-breadcrumbs';
