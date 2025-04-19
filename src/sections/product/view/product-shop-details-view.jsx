185df5a9315a26225f5ce920b48ecc3de009664a
Mrafisyahbani77 <mrafi.syahbani@gmail.com>
2025-04-19 20:09:52 +0700

    feat: fix error deploy vercel

--- src/sections/product/view/product-shop-details-view.jsx
+++ src/sections/product/view/product-shop-details-view.jsx
@@ -3,17 +3,14 @@ import { useCallback, useState } from 'react';
 // @mui
 import { alpha } from '@mui/material/styles';
 import Tab from '@mui/material/Tab';
-import Box from '@mui/material/Box';
 import Tabs from '@mui/material/Tabs';
 import Card from '@mui/material/Card';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Unstable_Grid2';
-import Typography from '@mui/material/Typography';
 // routes
 import { paths } from 'src/routes/paths';
 import { RouterLink } from 'src/routes/components';
-import { useGetProduct } from 'src/api/product';
 // components
 import Iconify from 'src/components/iconify';
 import EmptyContent from 'src/components/empty-content';
